52ca03b020e00b68d72e390ecb94c1f33ee884bc
【ＤＧ】ʀᴀᴠᴀɴ 🜲 <k68558742@gmail.com>
2026-05-25 09:02:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 
 load_dotenv()
 
-API_ID = int(os.getenv("21397343"))
-API_HASH = os.getenv("23f9c6deb3137f7910e0115e993436e0")
-BOT_TOKEN = os.getenv("8702357908:AAEtFKi02vDLmB6I6Ia8JvjRmQefPRneGf4")
+API_ID = int(os.getenv(21397343))
+API_HASH = os.getenv(23f9c6deb3137f7910e0115e993436e0)
+BOT_TOKEN = os.getenv(8702357908:AAEtFKi02vDLmB6I6Ia8JvjRmQefPRneGf4)
 
 app = Client(
     "mybot",
