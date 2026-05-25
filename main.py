3b9692335abca709c508c6aa777650bec8b1d4a4
【ＤＧ】ʀᴀᴠᴀɴ 🜲 <k68558742@gmail.com>
2026-05-25 08:56:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from pyrogram import Client, filters
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_ID = int(os.getenv("21397343"))
+API_HASH = os.getenv("23f9c6deb3137f7910e0115e993436e0")
+BOT_TOKEN = os.getenv("8702357908:AAEtFKi02vDLmB6I6Ia8JvjRmQefPRneGf4")
 
 app = Client(
     "mybot",
-    api_id=123456,
-    api_hash="your_api_hash",
-    bot_token="your_bot_token"
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
 )
 
 @app.on_message(filters.command("start"))
